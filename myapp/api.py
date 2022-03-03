fdfc3cee63d1ae06baa0f548172f1886cf790263
ohdowon064 <dhehdnjs03@gmail.com>
2022-03-03 20:59:02 +0900

    using api_operation to use mixed http method api

--- myapp/api.py
+++ myapp/api.py
@@ -1,7 +1,19 @@
+from django.http import HttpRequest
+from django.core.handlers.asgi import ASGIRequest
 from ninja import NinjaAPI
 
 api = NinjaAPI()
 
+
 @api.get("/hello")
-def hello(request):
+def hello(request: ASGIRequest):
     return "Hello World"
+
+
+@api.api_operation(["POST", "PATCH"], "/path")
+def mixed(request: ASGIRequest):
+    if request.method == "POST":
+        return "This is POST Path."
+
+    elif request.method == "PATCH":
+        return "This is PATCH Path."
