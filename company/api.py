f899070fb23dd27ea057c29f57aa85f59311e17c
ohdowon064 <dhehdnjs03@gmail.com>
2022-03-19 15:02:15 +0900

    List/Retrieve Employee 1. GET 요청에 대한 Out 스키마를 구현은 필수는 아니다. 2. 하지만 이것은 결과에 대한 validation, 자동 docs와 orm to json에 대한 자동 conversion을 제공한다.

--- company/api.py
+++ company/api.py
@@ -1,9 +1,11 @@
 import pprint
+from typing import List
 
-from ninja import Router, Body
+from django.shortcuts import get_object_or_404
+from ninja import Router, Body, Path
 
 from company.models import Employee
-from company.schema import EmployeeIn
+from company.schema import EmployeeIn, EmployeeOut
 
 router = Router()
 
@@ -12,3 +14,14 @@ def create_employee(request, payload: EmployeeIn = Body(...)):
     employee = Employee.objects.create(**payload.dict())
     pprint.pprint(employee.__dict__)
     return {"id": employee.id}
+
+@router.get("employees/{employee_id}/", response=EmployeeOut)
+def retrieve_employee(request, employee_id: int = Path(...)):
+    employee = get_object_or_404(Employee, id=employee_id)
+    return employee
+
+@router.get("employees/", response=List[EmployeeOut])
+def list_employees(request):
+    employee_qs = Employee.objects.all()
+    return employee_qs
+
